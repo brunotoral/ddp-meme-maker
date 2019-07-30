351f14ef388a8631d1e7da3ea30521b3c1b46a2e
Toral <brunotoral89@gmail.com>
2019-07-29 23:09:42 -0300

    Meme Model tests finished

--- spec/models/meme_spec.rb
+++ spec/models/meme_spec.rb
@@ -1,19 +1,29 @@
 require 'rails_helper'
 
 describe Meme do
-  it 'is invalid when top_text is blank' do
-    meme = Meme.new(top_text: '')
+  describe 'validations' do
+    it 'is invalid when top_text is blank' do
+      meme = Meme.new(top_text: '')
 
-    meme.validate
+      meme.validate
 
-    expect(meme.errors.messages[:top_text]).to include "can't be blank"
-  end
+      expect(meme.errors.messages[:top_text]).to include "can't be blank"
+    end
 
-  it 'is invalid when top_text is blank' do
-    meme = Meme.new(bottom_text: '')
+    it 'is invalid when top_text is blank' do
+      meme = Meme.new(bottom_text: '')
 
-    meme.validate
+      meme.validate
 
-    expect(meme.errors.messages[:bottom_text]).to include "can't be blank"
+      expect(meme.errors.messages[:bottom_text]).to include "can't be blank"
+    end
+
+    it 'is invalid when image is blank' do
+      meme = Meme.new(image: nil)
+
+      meme.validate
+
+      expect(meme.errors.messages[:image]).to include "invalid"
+    end
   end
 end
